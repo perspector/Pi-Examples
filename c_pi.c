734b4d8b02ae3f0200d117a03279499f2617d6ab
Benny <bennypythoncoder@gmail.com>
2021-03-04 13:51:02 -0500

    Update c_pi.c

--- c_pi.c
+++ c_pi.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 printf("How many digits of pi do you want to know? ");
-scanf("%d",&i)
-double F (int i) {
-    return 1 + i / (2.0 * i + 1) * F(i + 1);
+scanf("%d",&n)
+double F (int n) {
+    return 1 + n / (2.0 * n + 1) * F(n + 1);
 }
 PI = 2 * F(1);
 
 //i iterations in for loop, always odd, start val=3, end val=i, by/amount changed each time to make it odd=2//
 //x is arccot value 5 - 369//
+for(i=3;i>n;i+2) {
 4*(4*(1/x)-1/(i(x)**i) +1/(i(x)**i) )
+}
