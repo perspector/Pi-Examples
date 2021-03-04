930d3b65b7c727fb5348c59ac9021e9d09e58bdd
Benny <bennypythoncoder@gmail.com>
2021-03-04 12:00:50 -0500

    Update c_pi.c

--- c_pi.c
+++ c_pi.c
@@ -6,3 +6,7 @@ double F (int i) {
     return 1 + i / (2.0 * i + 1) * F(i + 1);
 }
 PI = 2 * F(1);
+
+//i iterations in for loop, always odd, start val=3, end val=i, by/amount changed each time to make it odd=2//
+//x is arccot value 5 - 369//
+4*(4*(1/x)-1/(i(x)**i) +1/(i(x)**i) )
